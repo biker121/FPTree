c858c397fe95c72dfe18e278b2941e9e41b363f6
Brahmdeep Singh Juneja <biker121@gmail.com>
2013-11-21 17:15:36 -0600

    Initial Commit

---
+++ 4710-Project-FrequentItemMining/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  4710-Project-FrequentItemMining
+//
+//  Created by Brahmdeep Singh Juneja on 11/21/2013.
+//  Copyright (c) 2013 Brahmdeep Singh Juneja. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
